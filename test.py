b0419a027072d84ec1c847d0cfa68a7404a8d597
Ghukii <68664943+Ghukii@users.noreply.github.com>
2024-03-19 19:52:39 +0300

    1
    qw123

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.google.com/search?q=site%3Avk.com&sca_esv=57c1abd91ece7b5a&ei=csH5Za7nLNX-7_UP7pueuAo&udm=&ved=0ahUKEwiu6KPA44CFAxVV_7sIHe6NB6cQ4dUDCBA&uact=5&oq=site%3Avk.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiC3NpdGU6dmsuY29tSMIlUMMJWOIjcAB4ApABAJgBhgagAYQhqgENNC4zLjEuMS4wLjMuMbgBA8gBAPgBAZgCB6ACqRfCAgQQABhHwgIKEAAYgAQYigUYQ8ICCxAuGIAEGMcBGNEDwgIFEC4YgATCAgUQABiABMICCBAuGIAEGNQCwgIOEC4YgAQYxwEY0QMY1ALCAhcQLhiABBjUAhiXBRjcBBjeBBjgBNgBAcICFBAuGIAEGJcFGNwEGN4EGOAE2AEBmAMAiAYBkAYIugYGCAEQARgUkgcLMS4xLjEuMS4wLjOgB5w3&sclient=gws-wiz-serp')
+
+
